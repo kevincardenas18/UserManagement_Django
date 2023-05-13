df89ba6b5b68813d28998434122d91ebbc29a944
Kevin Cárdenas <kevin.cardenas-1@outlook.com>
2023-05-13 15:42:17 -0500

    List bug

--- users/models.py
+++ users/models.py
@@ -7,6 +7,9 @@ class Mesa(models.Model):
     estado = models.BooleanField(default=False)
     capacidad = models.IntegerField()
 
+    def __str__(self):
+        return str(self.numeroMesa)
+
 def get_default_mesa():
     return Mesa.objects.filter(estado=False).first()
 
@@ -15,26 +18,40 @@ class Cliente(models.Model):
     telefono = models.CharField(max_length=10)
     direccion = models.TextField(blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    def __str__(self):
+        return self.cedula
+    
 class Reserva(models.Model):
     fecha = models.DateField()
     hora = models.TimeField()
     numPersonas = models.IntegerField()
     reserva = models.ForeignKey(User, on_delete=models.CASCADE)
     mesa = models.ForeignKey(Mesa, on_delete=models.CASCADE, default=get_default_mesa)
+    def __str__(self):
+        return str(self.fecha)
 
 class Categoria(models.Model):
     categoria = models.CharField(max_length=40)
+    def __str__(self):
+        return self.categoria
+
 
 class Menu(models.Model):
     nombre = models.CharField(max_length=40)
     precio = models.FloatField()
     description = models.TextField(blank=True)
     categoria_menu = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre
+
 
 class Pedido(models.Model):
     reserva = models.ForeignKey(Reserva, on_delete=models.CASCADE)
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.reserva)
+
+
 
 class Factura(models.Model):
     fecha = models.DateField()
@@ -42,6 +59,9 @@ class Factura(models.Model):
     subtotal = models.FloatField()
     iva = models.FloatField()
     pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.fecha)
+
     
 
 
