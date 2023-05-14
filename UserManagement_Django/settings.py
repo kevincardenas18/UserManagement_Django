95ed5923d5e9c9589b36ac66626e333565d1c24c
Kevin Cárdenas <kevin.cardenas-1@outlook.com>
2023-05-13 20:20:48 -0500

    Update settings.py

--- UserManagement_Django/settings.py
+++ UserManagement_Django/settings.py
@@ -80,9 +80,14 @@ WSGI_APPLICATION = 'UserManagement_Django.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-
-    )
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "core_db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": 5432,
+    }
 }
 
 
@@ -122,12 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # Following settings only make sense on production and may break development environments.
-if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
