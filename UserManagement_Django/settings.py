92926d0fb6e01a5a6ee10619616804b2c8d8a92a
Kevin Cárdenas <kevin.cardenas-1@outlook.com>
2023-05-13 23:33:33 -0500

    Add files

--- UserManagement_Django/settings.py
+++ UserManagement_Django/settings.py
@@ -17,19 +17,23 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+SECRET_KEY = 'django-insecure-0_q#rpe29fa*rx=z9+@ve^q200zc4q71$zi$ztrihq^8nar08v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ 
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+
 
 # Application definition
 
@@ -79,7 +83,7 @@ WSGI_APPLICATION = 'UserManagement_Django.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/core_db',        conn_max_age=600    )
+    'default': dj_database_url.config(default=env('DATABASE_URL'))
     }
 
 
